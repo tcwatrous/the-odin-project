0bedc3066c77d816cedc3ac67cbbfa1860faa6db
tcw4 <twatrous@gmail.com>
2014-04-23 09:11:37 -0700

    jquery first

---
+++ jquery-project/script.js
@@ -0,0 +1,40 @@
+
+
+$(document).ready(function(){
+
+	$('#trigger').click(function(){ 
+		$('#container').remove(); //prevent multiple grids
+		
+
+		var containerSide = 800;
+		var quantity = $('#num').val();
+		var hw = containerSide/quantity;
+
+		if(quantity>1 && quantity<129) { 
+			//new grid
+			$('body').append("<div id='container' style='width:"+containerSide+"px; height:"+containerSide+";'></div>");
+
+			//create new boxes within grid
+			for(var i=0 ; i< quantity ; i++) {
+				$('#container').append("<div class='row'>");
+				for(var j=0 ; j < quantity ; j++) {
+					$('#container').append("<div class='box' style='width:"+hw+"px; height:"+hw+"px; background-color:#888; display:inline-block;'></div>");
+				}
+				$('#container').append("</div>");
+			}
+		}
+		else {
+			alert('lolno');
+		}
+		$('.box').mouseover(function() {
+			$(this).css('background-color', '#00f');
+		});
+		$('.box').mouseout(function() {
+			$(this).css('background-color', '#888');
+		});	
+	});
+
+	
+});
+
+
